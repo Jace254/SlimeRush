df9f1f5e4c7dbd9635830c2fa9e7632e819e6d9c
Jace254 <joashmacer@gmail.com>
2022-05-07 13:40:56 +0300

    Completed character movement and animation

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ window.addEventListener('load', function()
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
-    canvas.height = 720;
+    canvas.height = 800;
     perfectFrameTime = 1000/60 ;
     //handle key events
     class InputHandler
-    
     {
         constructor(){
             this.keys = [];
             window.addEventListener('keydown', e => 
             {
+                console.log(e.key);
                 if((e.key === "ArrowDown" ||
                     e.key === "ArrowUp" ||
                     e.key === "ArrowLeft" ||
-                    e.key === "ArrowRight" ) 
+                    e.key === "ArrowRight" ||
+                    e.key === " ") 
                     && this.keys.indexOf(e.key) === -1)
                 {
                     this.keys.push(e.key);
@@ -27,7 +28,8 @@ window.addEventListener('load', function()
                 if( e.key === "ArrowDown" ||
                     e.key === "ArrowUp" ||
                     e.key === "ArrowLeft" ||
-                    e.key === "ArrowRight" ) 
+                    e.key === "ArrowRight" ||
+                    e.key === " ") 
                 {
                     this.keys.splice(this.keys.indexOf(e.key), 1);
                 }
@@ -45,16 +47,19 @@ window.addEventListener('load', function()
             this.height = 75;
             this.x = 0;
             this.y = this.gameHeight - this.height;
-            this.image = document.getElementById("playerImage3");
-            this.FrameX = 0 ;
+            this.image = document.getElementById("playerImage1");
+            this.FrameX = 8 ;
             this.FrameY = 0 ;
-            this.maxFrame = 7;
-            this.fps = 7;
+            this.maxFrame = 0;
+            this.minFrame = 0;
+            this.startFrame = 0;
+            this.fps = 5;
             this.frameTimer = 0;
             this.frameInterval = 1000/this.fps;
             this.speed = 0;
             this.vy = 0;
             this.gravity = 1;
+            this.direction = 1;
         }
         draw(context)
         {
@@ -63,51 +68,77 @@ window.addEventListener('load', function()
         //Player Movement
         update(input, deltaTime)
         {
-            //handle animation
-            if (this.frameTimer > this.frameInterval){
-                if(this.FrameX >= this.maxFrame) this.FrameX = 0;
-                else this.FrameX ++;
-                this.frameTimer = 0;
-            } else{
-                this.frameTimer += deltaTime;
-            }
-
             //handle input
             if(input.keys.indexOf("ArrowRight") > -1)
             {
                 this.speed = 5;
+                this.direction = 1;
+
             }             
             else if(input.keys.indexOf("ArrowLeft") > -1)
             {
-                this.speed = -5;   
+                this.speed = -5;
+                this.direction = -1;
+
+
             }
-            else if(input.keys.indexOf("ArrowUp") > -1 && this.isGrounded())
+            else if(( input.keys.indexOf(' ') > -1 || 
+                      input.keys.indexOf("ArrowUp") > -1)  
+                      && this.isGrounded())
             {
                 this.vy -= 30;
-                this.maxFrame = 5;
+                this.gameFrame = 6;
+
             }
             else {
                 this.speed = 0;
+
             }
+            //handle animation
+            if (this.frameTimer > this.frameInterval )
+            {
+                if(this.direction == 1){
+                    this.startFrame = 8;
+                    if(this.FrameX >= this.maxFrame) this.FrameX = 8;    
+                    else
+                    {
+                        if(this.FrameX < this.startFrame) this.FrameX = 8;
+                        this.FrameX  += this.direction 
+                    }
+                } else {
+                    this.startFrame = 7;
+                    if(this.FrameX <= this.minFrame) this.FrameX = 7;
+                    else
+                    {
+                        if(this.FrameX > this.startFrame) this.FrameX = 7
+                        this.FrameX  += this.direction 
+                    }
+                }
+
+
+                this.frameTimer = 0;
+            } else this.frameTimer += deltaTime;
+            
             //Horizontal Movement
             this.x += this.speed;
             if(this.x < 0) this.x = 0;
             else if(this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+
             //Vertical Movement
             this.y += this.vy;
             if(!this.isGrounded())
             {
                 this.vy += this.gravity;
                 this.FrameY = 0;
+                this.maxFrame = 13;
+                this.minFrame = 2;
             } 
             else
             {
                 this.vy = 0;
                 this.FrameY = 1;
-            }
-            if(this.vy >= 1)
-            {
-                this.FrameY = 0;
+                this.maxFrame = 15;
+                this.minFrame = 0;
             }
             if(this.y >= this.gameHeight - this.height) this.y = this.gameHeight - this.height;
         }
@@ -126,7 +157,7 @@ window.addEventListener('load', function()
     function displayStatusText()
     {}
 
-    const input = new InputHandler(0);
+    const input = new InputHandler();
     const player = new Player(canvas.width,canvas.height);
 
     let lastTime = 0
